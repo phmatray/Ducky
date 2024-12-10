6b7993ce1f566818ae6bd5b5533f651478d1a834
Philippe Matray <phmatray@gmail.com>
2024-12-10 12:57:41 +0100

    refactor: merge AddDucky methods

--- src/library/Ducky.Blazor/DependencyInjections.cs
+++ src/library/Ducky.Blazor/DependencyInjections.cs
@@ -17,38 +17,25 @@ public static class DependencyInjections
     /// Adds Ducky services to the specified <see cref="IServiceCollection"/>. This method registers the BlazorR3 services, dispatcher, slices, and effects.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
-    /// <param name="configuration">The application configuration.</param>
+    /// <param name="configuration">The application configuration (optional).</param>
     /// <param name="configureOptions">An optional action to configure the <see cref="DuckyOptions"/>.</param>
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
     public static IServiceCollection AddDucky(
         this IServiceCollection services,
-        IConfiguration configuration,
+        IConfiguration? configuration = null,
         Action<DuckyOptions>? configureOptions = null)
     {
         // Configure options
         DuckyOptions options = new();
-        configuration.GetSection("Ducky").Bind(options); // Bind configuration section to DuckyOptions
+
+        configuration?.GetSection("Ducky").Bind(options); // Bind configuration section to DuckyOptions
         configureOptions?.Invoke(options);
 
-        // Add Reactive Extensions for Blazor
-        services.AddScoped<TimeProvider, SynchronizationContextTimeProvider>();
-
-        return services.AddDuckyCore(options);
-    }
-
-    /// <summary>
-    /// Adds Ducky services to the specified <see cref="IServiceCollection"/>. This method registers the BlazorR3 services, dispatcher, slices, and effects.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
-    /// <param name="configureOptions">An optional action to configure the <see cref="DuckyOptions"/>.</param>
-    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-    public static IServiceCollection AddDucky(
-        this IServiceCollection services,
-        Action<DuckyOptions>? configureOptions = null)
-    {
-        // Configure options
-        DuckyOptions options = new();
-        configureOptions?.Invoke(options);
+        // Add Reactive Extensions for Blazor if configuration is provided
+        if (configuration is not null)
+        {
+            services.AddScoped<TimeProvider, SynchronizationContextTimeProvider>();
+        }
 
         return services.AddDuckyCore(options);
     }
