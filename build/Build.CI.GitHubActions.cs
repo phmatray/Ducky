2845396e0982b9a34d2980090fafea571d473634
Philippe Matray <phmatray@gmail.com>
2024-12-31 13:12:31 +0100

    ci: prepare first release

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -1,30 +1,30 @@
 using Nuke.Common.CI.GitHubActions;
 
-[GitHubActions(
-    "windows-latest",
-    GitHubActionsImage.WindowsLatest,
-    FetchDepth = 0,
-    OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
-    OnPullRequestBranches = [DevelopBranch],
-    InvokedTargets = [nameof(Tests), nameof(Pack)],
-    PublishArtifacts = false,
-    AutoGenerate = false)]
-[GitHubActions(
-    "macos-latest",
-    GitHubActionsImage.MacOsLatest,
-    FetchDepth = 0,
-    OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
-    OnPullRequestBranches = [DevelopBranch],
-    InvokedTargets = [nameof(Tests), nameof(Pack)],
-    PublishArtifacts = false,
-    AutoGenerate = false)]
-[GitHubActions(
-    "ubuntu-latest",
-    GitHubActionsImage.UbuntuLatest,
-    FetchDepth = 0,
-    OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
-    OnPullRequestBranches = [DevelopBranch],
-    InvokedTargets = [nameof(Tests), nameof(Pack)],
-    PublishArtifacts = false,
-    AutoGenerate = false)]
-partial class Build;
+// [GitHubActions(
+//     "windows-latest",
+//     GitHubActionsImage.WindowsLatest,
+//     FetchDepth = 0,
+//     OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
+//     OnPullRequestBranches = [DevelopBranch],
+//     InvokedTargets = [nameof(UnitTests), nameof(Pack)],
+//     PublishArtifacts = false,
+//     AutoGenerate = false)]
+// [GitHubActions(
+//     "macos-latest",
+//     GitHubActionsImage.MacOsLatest,
+//     FetchDepth = 0,
+//     OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
+//     OnPullRequestBranches = [DevelopBranch],
+//     InvokedTargets = [nameof(UnitTests), nameof(Pack)],
+//     PublishArtifacts = false,
+//     AutoGenerate = false)]
+// [GitHubActions(
+//     "ubuntu-latest",
+//     GitHubActionsImage.UbuntuLatest,
+//     FetchDepth = 0,
+//     OnPushBranchesIgnore = [MainBranch, $"{ReleaseBranchPrefix}/*"],
+//     OnPullRequestBranches = [DevelopBranch],
+//     InvokedTargets = [nameof(UnitTests), nameof(Pack)],
+//     PublishArtifacts = false,
+//     AutoGenerate = false)]
+// partial class Build;
