12abdf7e7311bea3f2a876630157507b3aae0f7d
Philippe Matray <phmatray@gmail.com>
2025-06-11 20:05:16 +0200

    fix: some implicit namespaces does not work

--- src/demo/Demo.BlazorWasm/GlobalUsings.cs
+++ src/demo/Demo.BlazorWasm/GlobalUsings.cs
@@ -1,9 +1,17 @@
 // Global using directives
 
+global using System;
+global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Globalization;
+global using System.Linq;
+global using System.Net.Http;
 global using System.Text;
 global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
 global using Demo.BlazorWasm.Components.Shared;
 global using Demo.BlazorWasm.Features.Feedback.Actions;
 global using Demo.BlazorWasm.Features.JsonColoring.Helpers;
